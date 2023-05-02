feee5b55c74a65c4f696fe70b535942748805d17
soufianeb7 <soufiane0.0bouhasni@gmail.com>
2023-05-03 00:42:48 +0100

    handle 9 errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,16 +4,17 @@
  * insert_nodeint_at_index - inserts a new node in a linked-list,
  * at a given posts
  * @head: pointer to the first node in the list
- * @indx: index  postion of  the new node
+ * @idx: index  postion of  the new node
  * @n: data to insert in the new node
  *
  * Return: pointer to the new node, or null
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int indx, int n)
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
-	listint_t *tmp = *head;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
@@ -22,25 +23,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int indx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (indx == 0)
+	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
 
-	for (i = 0; tmp && i < indx; i++)
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (i == indx - 1)
+		if (i == idx - 1)
 		{
-			new->next = tmp->next;
-			tmp->next = new;
+			new->next = temp->next;
+			temp->next = new;
 			return (new);
 		}
 		else
-			tmp = tmp->next;
+			temp = temp->next;
 	}
 
 	return (NULL);
 }
+
 
